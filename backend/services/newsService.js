41f7d0223a662845a39c28b2b991a785b899a305
Osmancan Bozali <osmancanbozali@gmail.com>
2024-12-11 18:26:35 +0300

    change gnews api query

--- backend/services/newsService.js
+++ backend/services/newsService.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // category, max, lang, expand, from, to, country???
 const fetchNewsFromAPI = async (category,newsCount) => {
     try {
-        const response = await axios.get(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=${process.env.GNEWS_API_KEY}&lang=en&max=${newsCount}`);
+        const response = await axios.get(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=${process.env.GNEWS_API_KEY}&lang=en&max=${newsCount}&expand=content`);
         return response.data.articles;
     } catch (error) {
         console.error('Error fetching news from API:', error);
