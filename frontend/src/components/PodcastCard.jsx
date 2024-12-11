6cf039aa564056b4d53474f9fb9a513d73268174
Osmancan Bozali <osmancanbozali@gmail.com>
2024-12-11 18:11:20 +0300

    quick fix podcastCard

--- frontend/src/components/PodcastCard.jsx
+++ frontend/src/components/PodcastCard.jsx
@@ -2,7 +2,14 @@ import { useState, useEffect } from "react";
 import MediaPlayer from "./MediaPlayer";
 export default function PodcastCard() {
     const [podcastUrl, setPodcastUrl] = useState("");
+    const [podcastDate, setPodcastDate] = useState("");
     const [loading, setLoading] = useState(true);
+
+    const formatDate = (podcastDate) => {
+        const date = new Date(podcastDate);
+        const options = { day: '2-digit', month: 'short', year: 'numeric' };
+        return new Intl.DateTimeFormat('en-GB', options).format(date);
+    }
 
     useEffect(() => {
         const fetchPodcast = async () => {
@@ -18,6 +25,7 @@ export default function PodcastCard() {
                 );
                 const data = await response.json();
                 setPodcastUrl(data.podcast.podcastUrl);
+                setPodcastDate(data.podcast.createdAt);
             } catch (error) {
                 console.error("Error fetching podcast data: ", error);
             } finally {
@@ -30,11 +38,10 @@ export default function PodcastCard() {
     return (
         <div className="flex flex-col items-center md:flex-row gap-4 mt-10 mx-6 md:mx-4 p-6 md:p-4 h-fit bg-maincolor rounded-2xl">
             <div className="bg-white rounded-2xl flex w-1/2 md:w-1/4 aspect-square flex-col justify-center items-center">
-                <h1 className="text-4xl md:text-6xl text-maincolor">24 Nov</h1>
-                <h1 className="text-4xl md:text-6xl text-maincolor">2024</h1>
+                <h1 className="text-4xl md:text-5xl text-maincolor text-center">{podcastDate ? formatDate(podcastDate) : "Daily Podcast"}</h1>
             </div>
             <div className="flex flex-col w-full h-full  justify-between">
-                <h1 className="text-white text-xl md:text-2xl text-center md:text-left md:mt-2">Your daily Podcast is here! Listen it now to catch up with the yesterday’s most clicked news.</h1>
+                <h1 className="text-white text-xl md:text-2xl text-center md:text-left md:mt-2">{podcastUrl ? "Your daily Podcast is here! Listen it now to catch up with the yesterday’s most clicked news." : "Your daily podcast is not available at the moment!, Please come back tomorrow."}</h1>
                 {loading ? <h1 className="text-white text-xl md:text-2xl text-center md:text-left md:mt-2">Loading...</h1> : <MediaPlayer audioUrl={podcastUrl} />}
             </div>
         </div>
